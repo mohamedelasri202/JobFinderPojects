c50a0b135ee8d91ce6743da72d5fa3e8f0673f78
mohamedelasri202 <el7994995@gmail.com>
2026-02-09 14:27:57 +0100

    adding the register methode in the service

--- src/app/services/auth/auth.ts
+++ src/app/services/auth/auth.ts
@@ -1,15 +1,13 @@
-// import { inject, Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class Auth {
-//   private http = inject(HttpClient);  
-//   private apiUrl = 'https//:localhost:3000'
-
-
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+  private http = inject(HttpClient);  
+  private apiUrl = 'https//:localhost:3000'
 
 
 
@@ -21,9 +19,11 @@
 
 
 
-//   register(userData:any):Observable<any>{
-//     return this.http.post(`${this.apiUrl}/users`,userData);
-//   }
 
 
-// }
+  register(userData:any):Observable<any>{
+    return this.http.post(`${this.apiUrl}/users`,userData);
+  }
+
+
+}
