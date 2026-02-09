1efd01aad2f7e6591e5a1bda481b07011e0a117d
mohamedelasri202 <el7994995@gmail.com>
2026-02-09 14:25:49 +0100

    addig the component for the login

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,13 +1,13 @@
-// import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
-// @Component({
-//   selector: 'app-login',
-//   imports: [],
-//   templateUrl: './login.html',
-//   styleUrl: './login.css',
-// })
-// export class Login {
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
 
   
 
-// }
+}
