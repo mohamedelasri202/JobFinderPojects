064b1c8429f5fb37e5744f1b97bcd9334988f1c2
mohamedelasri202 <el7994995@gmail.com>
2026-02-09 14:15:03 +0100

    feat: add auth service methods

--- src/app/services/auth/auth.ts
+++ src/app/services/auth/auth.ts
@@ -1,8 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../../modules/user/user-module';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Auth {
-  
+export class AuthService {
+
+  private apiUrl = 'http://localhost:3000/users';
+
+  constructor(private http: HttpClient) { }
+
+  register(user: User): Observable<User> {
+    return this.http.post<User>(this.apiUrl, user);
+  }
+
+  login(email: string, password: string): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}?email=${email}&password=${password}`);
+  }
 }
