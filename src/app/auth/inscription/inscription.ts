757944b0eefbdec9377bb2e100f387937737f34b
mohamedelasri202 <el7994995@gmail.com>
2026-02-09 14:24:43 +0100

    addig the component for the inscription

--- src/app/auth/inscription/inscription.ts
+++ src/app/auth/inscription/inscription.ts
@@ -1,15 +1,15 @@
-// import { Component, inject } from '@angular/core';
-// import { Form, FormBuilder, FormGroup, FormsModule,Validator, Validators } from '@angular/forms';
-// import { Auth } from '../../services/auth/auth';
-// import { email, required } from '@angular/forms/signals';
+import { Component, inject } from '@angular/core';
+import { Form, FormBuilder, FormGroup, FormsModule,Validator, Validators } from '@angular/forms';
+import { Auth } from '../../services/auth/auth';
+import { email, required } from '@angular/forms/signals';
 
-// @Component({
-//   selector: 'app-inscription',
-//   imports: [FormsModule],
-//   templateUrl: './inscription.html',
-//   styleUrl: './inscription.css',
-// })
-// export class Inscription {
+@Component({
+  selector: 'app-inscription',
+  imports: [FormsModule],
+  templateUrl: './inscription.html',
+  styleUrl: './inscription.css',
+})
+export class Inscription {
 
 //   private authservice =inject(Auth)
 //   registerForm!:FormGroup
@@ -36,4 +36,4 @@
 
 //   }
 
-// }
+}
