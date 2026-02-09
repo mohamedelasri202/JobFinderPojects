94417f3436736804844c21c6671f86b27ad913f1
mohamedelasri202 <el7994995@gmail.com>
2026-02-09 14:27:30 +0100

    adding the on submet methode and the methode to get the data from the inscription form in the componenet

--- src/app/auth/inscription/inscription.ts
+++ src/app/auth/inscription/inscription.ts
@@ -11,29 +11,29 @@ import { email, required } from '@angular/forms/signals';
 })
 export class Inscription {
 
-//   private authservice =inject(Auth)
-//   registerForm!:FormGroup
+  private authservice =inject(Auth)
+  registerForm!:FormGroup
 
-//     constructor(private fb: FormBuilder) {}
+    constructor(private fb: FormBuilder) {}
 
 
-//     ngOnInit():void{
-//       this.registerForm =this.fb.group({
-//         userName:['',[Validators.required,Validators.pattern(/^[a-zA-Z0-9_]{3,}$/)]],
-//         email:['',[Validators.required,Validators.email]],
-//         password:['',[Validators.required,Validators.minLength(6)]]
-//       })
+    ngOnInit():void{
+      this.registerForm =this.fb.group({
+        userName:['',[Validators.required,Validators.pattern(/^[a-zA-Z0-9_]{3,}$/)]],
+        email:['',[Validators.required,Validators.email]],
+        password:['',[Validators.required,Validators.minLength(6)]]
+      })
 
-//     }
+    }
 
 
 
     
-//   onSubmet(){
-//     if(this.registerForm.invalid){
-//       this.registerForm.markAllAsTouched();
-//     }
+  onSubmet(){
+    if(this.registerForm.invalid){
+      this.registerForm.markAllAsTouched();
+    }
 
-//   }
+  }
 
 }
