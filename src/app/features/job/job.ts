96c432594c4bc8085fd53ced04e3c7caab918766
mohamedelasri202 <el7994995@gmail.com>
2026-02-10 12:29:30 +0100

    adding the structure for the job

---
+++ src/app/features/job/job.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job',
+  imports: [],
+  templateUrl: './job.html',
+  styleUrl: './job.css',
+})
+export class Job {
+
+}
